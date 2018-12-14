2cc170a7d5106c23be502800cb807ba310ce7d0b
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2018-12-14 03:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ function shout(string) { return string.toUpperCase
 
 function whisper(string) {return string.toLowerCase() }
 
+var uppercase = "HELLO!"
+
+function logShout(string)
+ { return console.log(string.toUpperCase()) }
+
+
+function logWhisper(string)
+{ return console.log(string.toLowerCase() ) }
+
+function sayHitoGrandma(string)
+
+{
+ if
+  (string === string.toLowerCase()) { return("I can\'t hear you!") }
+else if
+(string === string.toUpperCase()) { return("YES INDEED!")}
+else if
+(string.localeCompare("I love you, Grandma")) { return ("I love you, too.") }
+}
+
+
